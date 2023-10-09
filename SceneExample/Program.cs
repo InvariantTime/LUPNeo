b61f3ea4bd7fef6e6b68699c9d485952e30b3713
InvariantTime <87831309+InvariantTime@users.noreply.github.com>
2023-10-09 18:46:11 +0300

    scene example

---
+++ SceneExample/Program.cs
@@ -0,0 +1,48 @@
+﻿using LUP.DependencyInjection;
+using LUP.SceneGraph;
+using LUP.Client;
+using LUP;
+using LUP.SceneGraph.Components;
+using LUP.Graphics.OpenGL;
+
+var builder = new ApplicationBuilder();
+builder.Services.AddSceneSystem();
+builder.Services.AddOpenGL();
+
+builder.Services.AddDesktopWindow(x => {
+
+    x.Title = "Hello window!";
+    x.Visible = true;
+    x.Resizable = true;
+});
+
+ var app = builder.Build();
+app.AddStage<ISceneProcessor>();
+app.AddStage<IWindowProcessor>();
+
+var sceneP = app.Services.GetService<ISceneProcessor>()!;
+sceneP.LoadScene(null);
+var scene = sceneP.Provider!.Scene;
+var obj = scene.GetRootObjects().Instantiate(null);
+
+var component = new SuperComponent();
+obj.AddComponent(component);
+component.Destroy();
+
+
+app.Run();
+
+
+class SuperComponent : ComponentBase
+{
+    protected override void OnInitialized()
+    {
+        Console.WriteLine("INIT");
+    }
+
+
+    protected override void OnUnitialized()
+    {
+        Console.WriteLine("UNINIT");
+    }
+}
