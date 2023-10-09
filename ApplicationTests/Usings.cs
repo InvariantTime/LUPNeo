9da8e88890b5a0dcf33afb1739ca1b495dc8dc17
InvariantTime <87831309+InvariantTime@users.noreply.github.com>
2023-10-09 23:28:40 +0300

    app tests

--- ApplicationTests/Usings.cs
+++ ApplicationTests/Usings.cs
@@ -1,3 +1,3 @@
-global using Microsoft.VisualStudio.TestTools.UnitTesting;
-global using LUP.DependencyInjection;
 global using LUP;
+global using LUP.DependencyInjection;
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
