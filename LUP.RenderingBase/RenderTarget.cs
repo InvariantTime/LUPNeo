782ff06ea969eeed8e38773130c99f34139fd5b5
InvariantTime <87831309+InvariantTime@users.noreply.github.com>
2024-04-21 00:21:09 +0300

    Add render target

---
+++ LUP.RenderingBase/RenderTarget.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LUP.Rendering
+{
+    public class RenderTarget
+    {
+        public string Name { get; }
+
+        public RenderTarget()
+        {
+
+        }
+    }
+}
